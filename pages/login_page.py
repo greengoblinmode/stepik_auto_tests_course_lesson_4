d249d17d7038b56fa476602062d614d040771ec2
NORBIT\Kirill.Dubyna <kirill.dubyna@norbit.ru>
2023-12-31 20:36:54 +0300

    Реализация LoginPage (доработка)

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,31 +4,20 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class LoginPage(BasePage):
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "Login page URL is incorrect"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Форма авторизации не отображается"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "Форма регистрации не отображается"
+
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
-
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        try:
-            assert "login" in self.browser.current_url()
-        except:
-            return False
-        assert True
-
-    def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        try:
-            self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
-        except (NoSuchElementException):
-            return False
-        assert True
-
-    def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        try:
-            self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
-        except (NoSuchElementException):
-            return False
-        assert True
--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    page.should_be_login_link()
+    url = "http://selenium1py.pythonanywhere.com/"
+    main_page = MainPage(browser, url)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    login_page = LoginPage(browser, url)
+    main_page.open()                      # открываем страницу
+    main_page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    main_page.should_be_login_link()
+    time.sleep(2)
+    login_page.should_be_login_page()
